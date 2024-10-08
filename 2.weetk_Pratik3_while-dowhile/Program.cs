3a0cdd9c50cfbe0edd23d610e7b19e88e61b759b
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-08 13:58:58 +0300

    Proje dosyası ekle.

---
+++ 2.weetk_Pratik3_while-dowhile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //while döngüsü ile sayaç arttırarak yazdırma
+        Console.WriteLine("Lütfen bir limit değeri giriniz.");//Kullanıcıdın bir limit değeri alınır.
+        int limit = Convert.ToInt32(Console.ReadLine());//Convert.ToInt32 komutu ile kullanıcıdan alınan string değeri tamsayıya çevrilir.
+        int sayaç = 0;//Sayaç başlangıç değeri.
+
+        while (sayaç <= limit)//Limit aşılana kadar döngü devam eder.
+        {
+            Console.WriteLine("Ben bir Patika'lıyım.");//Ekrana mesajı yazdırır.
+            sayaç++;//Sayaç her döngüde bir artacak.
+
+        }
+
+        //do-while döngüsü ile sayaç arttırarak yazdırma
+        Console.WriteLine("Lütfen bir limit değeri giriniz.");//Kullanıcıdın bir limit değeri alınır.
+        int limit = Convert.ToInt32(Console.ReadLine());//Convert.ToInt32 komutu ile kullanıcıdan alınan string değeri tamsayıya çevrilir.
+        int sayaç = 0;//Sayaç başlangıç değeri.
+        do
+        {
+            Console.WriteLine("Ben bir Patika'lıyım.");//Ekrana mesajı yazdırır.
+            sayaç++;//Sayaç her döngüde bir artacak.
+        }
+        while (sayaç <= limit);//Limit aşılana kadar döngü devam eder.
+    }
+}
+          //while ile do-while arasındaki fark :while döngüsü koşulu döngünün başında kontrol ederken do-while için ise koşul döngü sonunda kontrol edilir.Bu neden do-while döngülerinde koşul en az 1 kez çalışırken while için böyle bir garanti vermez.
