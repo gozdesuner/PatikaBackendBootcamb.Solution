5e960054287e49c2dfbababb74c160601ee59728
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-21 23:41:39 +0300

    Constructor

---
+++ 4.Week_Pratik_2_Constructor/Bebek.cs
@@ -0,0 +1,31 @@
+﻿namespace _4.Week_Pratik_2_Constructor
+{
+    public class Bebek
+    {
+        private DateTime _birthday { get; set; }
+        private string _name { get; set; }
+        private string _surname { get; set; }
+
+        //Defauld constructor
+        public Bebek()
+        {
+            _birthday = DateTime.Now;// Doğum tarihi şu anki zaman
+            Console.WriteLine("Ingaaaaağğğğğğğğ");// Bebek oluşturulunca konsola yazılacak
+            _name = "Unknown";// Default değerler
+            _surname = "Unknown";
+        }
+
+        public Bebek(string _name, string _surname)
+        {
+            _birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaaağğğğğğğğ");
+            _name = _name;// Parametrelerden gelen ad ve soyadı atama
+            _surname = _surname;
+
+        }
+        public void Printİnfo()
+        {
+            Console.WriteLine($"Name:{_name},Surname:{_surname}, Birthday:{_birthday}");
+        }
+    }
+}
---
+++ 4.Week_Pratik_2_Constructor/Program.cs
@@ -0,0 +1,14 @@
+﻿using _4.Week_Pratik_2_Constructor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bebek baby1 = new Bebek();// 1. Bebek: Default constructor ile oluşturma
+        baby1.Printİnfo();
+
+        Bebek baby2 = new Bebek("Adrianna", "Jammy");// 2. Bebek: Parametreli constructor ile oluşturma
+        baby2.Printİnfo();
+        Console.ReadKey();
+    }
+}
