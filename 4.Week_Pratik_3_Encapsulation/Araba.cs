36d761630d8cd1f80c2f2e805d8c8df72680ed88
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-23 02:45:25 +0300

    Encapsulation

---
+++ 4.Week_Pratik_3_Encapsulation/Araba.cs
@@ -0,0 +1,42 @@
+﻿namespace _4.Week_Pratik_3_Encapsulation
+{
+    public class Araba
+    {
+        public string marka { get; set; }
+        public string model { get; set; }
+        public string renk { get; set; }
+
+        private int _KapiSayisi;
+
+        public int KapiSayisi
+        {
+            get { return _KapiSayisi; }
+
+            set// Bu kısım, dışarıdan KapiSayisina bir değer atanmak istendiğinde çalışır. value, atanan değeri temsil eder. Eğer value 2 veya 4 olursa, bu değer _KapiSayisi'na atanır.
+            {
+                if (value == 2 || value == 4)
+                {
+                    _KapiSayisi = value;
+                }
+                else//Eğer value 2 veya 4 değilse, bir uyarı mesajı görüntülenir ve _KapiSayisi'na -1 atanır. Bu, hatalı bir kapı sayısı olduğu anlamına gelir.
+                {
+                    Console.WriteLine("Geçersiz kapı sayısı! Lütfen 2 veya 4 girin.");
+                    _KapiSayisi = -1;
+
+                }
+            }
+
+        }
+
+        public Araba(string arabaMarka, string arabaModel, string arabaRenk, int kapiSayisi)//Bu, yapıcı metottur. Araba sınıfından yeni bir nesne oluşturulurken çalışır ve arabayla ilgili bilgilerin (marka, model, renk, kapı sayısı) atanmasını sağlar.
+        {
+            marka = arabaMarka;//Parametre olarak alınan arabaMarka değeri, sınıfın marka özelliğine atanır.
+            model = arabaModel;
+            renk = arabaRenk;
+            KapiSayisi = kapiSayisi;//Parametre olarak gelen kapiSayisi değeri, kapsülleme özelliğine (KapiSayisi) atanır ve bu sayede doğrulama kontrolü yapılır.
+
+
+        }
+
+    }
+}
---
+++ 4.Week_Pratik_3_Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using _4.Week_Pratik_3_Encapsulation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Araba araba1 = new Araba("Alfa Romeo", "Tonale", "Gri", 4);
+        Console.WriteLine($"Araba1:{araba1.marka},{araba1.model},{araba1.renk},Kapı Sayısı:{araba1.KapiSayisi}");
+
+        Araba araba2 = new Araba("Mercedes", "GLC", "Mavi", 5);
+        Console.WriteLine($"Araba2:{araba2.marka},{araba2.model},{araba2.renk},Kapı Sayısı:{araba2.KapiSayisi}");
+
+    }
+}
