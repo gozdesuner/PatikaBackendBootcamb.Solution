c050416dfc6d58009a355574bb5b0c51619f88c2
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-24 01:20:56 +0300

    Inheritance

---
+++ 4.Week_Pratik_5_Inheritance/BaseKisi.cs
@@ -0,0 +1,34 @@
+﻿namespace _4.Week_Pratik_5_Inheritance
+{
+    public class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void KisiBilsisi()
+        {
+            Console.WriteLine($"Ad:{Ad} Soyad:{Soyad}");
+        }
+
+    }
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrenciNo { get; set; }
+
+        public void OgrenciBilgisi()
+        {
+            KisiBilsisi();
+            Console.WriteLine($"Öğrenci No:{OgrenciNo}");
+        }
+    }
+    public class Ogretmen : BaseKisi
+    {
+        public decimal Maas { get; set; }
+
+        public void OgretmenBilgi()
+        {
+            KisiBilsisi();
+            Console.WriteLine($"Maaş: {Maas}TL");
+        }
+    }
+}
---
+++ 4.Week_Pratik_5_Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using _4.Week_Pratik_5_Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ogrenci ogrenci = new Ogrenci
+        {
+            Ad = "Ece",
+            Soyad = "Aydın",
+            OgrenciNo = 1888
+        };
+        ogrenci.OgrenciBilgisi();
+
+        Ogretmen ogretmen = new Ogretmen
+        {
+            Ad = "Caner",
+            Soyad = "Demiz",
+            Maas = 58000
+        };
+        ogretmen.OgretmenBilgi();
+    }
+}
