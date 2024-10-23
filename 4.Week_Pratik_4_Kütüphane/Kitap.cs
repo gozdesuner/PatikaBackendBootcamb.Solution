1f9b1f96d8473c3c428a3bf09d4fdd9863dc84b6
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-23 03:30:21 +0300

    Patika Kütüphanesi

---
+++ 4.Week_Pratik_4_Kütüphane/Kitap.cs
@@ -0,0 +1,38 @@
+﻿namespace _4.Week_Pratik_4_Kütüphane
+{
+    public class Kitap
+    {
+        //Properties
+        public string Ad { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyad { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime KayitTarihi { get; private set; }//Kayıt tarihi otomatik atanacak.
+
+        // Default constructor (parametresiz)
+        public Kitap()
+        {
+            KayitTarihi = DateTime.Now;
+
+        }
+        public Kitap(string ad, string yazarAdi, string yazarSoyad, int sayfaSayisi, string yayinevi)//Parametre alan constructor
+        {
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            YazarSoyad = yazarSoyad;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinevi;
+            KayitTarihi = DateTime.Now;// Kayıt tarihi otomatik atanıyor.
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine($"Kitabın Adı:{Ad}");
+            Console.WriteLine($"Yazar:{YazarAdi}{YazarSoyad}");
+            Console.WriteLine($"Sayfa Sayısı:{SayfaSayisi}");
+            Console.WriteLine($"Yayınevi:{Yayinevi}");
+            Console.WriteLine($"Kayıt Tarihi:{KayitTarihi}");
+        }
+    }
+}
---
+++ 4.Week_Pratik_4_Kütüphane/Program.cs
@@ -0,0 +1,11 @@
+﻿using _4.Week_Pratik_4_Kütüphane;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kitap kitap1 = new Kitap("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+        kitap1.Yazdir();
+
+    }
+}
