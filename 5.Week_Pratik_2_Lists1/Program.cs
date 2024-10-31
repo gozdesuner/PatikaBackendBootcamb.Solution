30271e59137d753e8478878393db401adbeb08d5
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-10-31 23:25:41 +0300

    Lists1

---
+++ 5.Week_Pratik_2_Lists1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> davetliler = new List<string>
+        {
+            "Bülent Ersoy",
+            "İbrahim Tatlıses",
+            "Müslüm Gürses",
+            "Güllü",
+            "Orhan Gencebay",
+            "Sezen Aksu",
+            "Azer Bülbül"
+        };
+
+        Console.WriteLine("**Davetliler**");
+        foreach (string isim in davetliler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+}
