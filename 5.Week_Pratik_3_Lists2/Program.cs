4c29519939c42682b78fcbef29462bc82ff5f57e
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-11-01 12:22:42 +0300

    Lists2

---
+++ 5.Week_Pratik_3_Lists2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Dictionary<int, string> coffee = new Dictionary<int, string>();// Kahve isimlerini saklayacak bir sözlük tanımladım.
+
+for (int i = 0; i < 5; i++)//for döngüsüyle kullanıcıdan isimler alır.Dictionary içine eklenir.
+{
+    Console.WriteLine($"lütfen{i + 1} kahve ismini giriniz:");
+    string name = Console.ReadLine();
+    coffee.Add(i + 1, name);//Add metodu iki parametre alır: bir anahtar (int türünde sıra numarası) ve bir değer (kahve ismi).
+
+}
+Console.WriteLine("Girdiğiniz kahve isimleri:");
+
+foreach (var s in coffee)//foreach döngüsünde coffee içindeki Value değerlerini (string kahve isimleri) yazdırıyoruz.
+{
+    Console.WriteLine(s);
+}
