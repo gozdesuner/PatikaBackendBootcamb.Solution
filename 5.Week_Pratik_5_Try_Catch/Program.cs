ee332d30f403cf8bef8e0c32b71901bd41076979
Gözde <Gzde@DESKTOP-C1CKA2K>
2024-11-02 15:06:54 +0300

    Try Catch

---
+++ 5.Week_Pratik_5_Try_Catch/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+
+        try
+        {
+            // Kullanıcının girdiği değeri double olarak parslıyoruz
+            double sayi = double.Parse(Console.ReadLine());
+
+            // Geçerli bir sayı girildiyse karesini hesaplayıp gösteriyoruz
+            double kare = sayi * sayi;
+            Console.WriteLine($"Girilen sayının karesi: {kare}");
+        }
+        catch (FormatException)
+        {
+            // Kullanıcı geçersiz bir giriş yaparsa hata mesajı gösteriyoruz
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
