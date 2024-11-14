73e654ca4b263b73e5ff80e6296e2dd149a05ca9
Gözde Süner <gzdsuner@gmail.com>
2024-11-14 15:34:47 +0300

    Patikaflix Dizi Platformu

---
+++ 7.Week_Pratik_3_Patikaflix_Dizi_Platformu/Dizi.cs
@@ -0,0 +1,37 @@
+﻿namespace _7.Week_Pratik_3_Patikaflix_Dizi_Platformu
+{
+    public class Dizi
+    {
+        public string DiziAd { get; set; }
+        public string YapimYili { get; set; }
+        public string Turu { get; set; }
+        public string YayinYili { get; set; }
+        public string Yönetmen { get; set; }
+        public string YayinlandigiIlkPlatform { get; set; }
+
+        public Dizi(string diziAd, string yapimYili, string turu, string yayinYili, string yönetmen, string yayinlandigiIlkPlatform)
+        {
+            DiziAd = diziAd;
+            YapimYili = yapimYili;
+            Turu = turu;
+            YayinYili = yayinYili;
+            Yönetmen = yönetmen;
+            YayinlandigiIlkPlatform = yayinlandigiIlkPlatform;
+        }
+    }
+
+    public class KomediDizisi
+    {
+        public string DiziAdi { get; set; }
+        public string DiziTuru { get; set; }
+        public string Yonetmen { get; set; }
+
+        public KomediDizisi(string diziAdi, string diziTuru, string yonetmen)
+        {
+            DiziAdi = diziAdi;
+            DiziTuru = diziTuru;
+            Yonetmen = yonetmen;
+        }
+
+    }
+}
---
+++ 7.Week_Pratik_3_Patikaflix_Dizi_Platformu/Program.cs
@@ -0,0 +1,56 @@
+﻿using _7.Week_Pratik_3_Patikaflix_Dizi_Platformu;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Dizi> diziler = new List<Dizi>();
+        string eklemeDevam;
+
+        do
+        {
+            Console.Write("Dizi Adı: ");
+            string diziAd = Console.ReadLine();
+
+            Console.Write("Yapım Yılı: ");
+            string yapimYili = Console.ReadLine();
+
+            Console.Write("Türü: ");
+            string türü = Console.ReadLine();
+
+            Console.Write("Yayın Yılı: ");
+            string yayinYili = Console.ReadLine();
+
+            Console.Write("Yönetmen: ");
+            string yönetmen = Console.ReadLine();
+
+            Console.Write("Yayınlandığı İlk Platform: ");
+            string yayinlandiğiIlkPlatform = Console.ReadLine();
+
+            diziler.Add(new Dizi(diziAd, yapimYili, türü, yayinYili, yönetmen, yayinlandiğiIlkPlatform));
+
+            Console.Write("Yeni bir dizi eklemek istiyor musunuz? (E/H): ");
+            eklemeDevam = Console.ReadLine().ToUpper();
+
+
+        }
+
+        while (eklemeDevam == "E");
+
+        List<KomediDizisi> komediDizileri = diziler
+            .Where(dizi => dizi.Turu.ToLower() == "komedi")
+            .Select(dizi => new KomediDizisi(dizi.DiziAd, dizi.Turu, dizi.Yönetmen))
+            .OrderBy(dizi => dizi.DiziAdi)
+            .ThenBy(dizi => dizi.Yonetmen)
+            .ToList();
+
+        Console.WriteLine("\nKomedi Dizileri Listesi:");
+        foreach (var dizi in komediDizileri)
+        {
+            Console.WriteLine($"Dizi Adı: {dizi.DiziAdi},Türü:{dizi.DiziTuru}, Yönetmen: {dizi.Yonetmen}");
+        }
+
+
+
+    }
+}
